0d71ca0389e4225161ab730d90f1b37fb8bd436c
conno <conno@CONSTRAPCUM>
2024-11-06 10:04:58 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment_1._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            Console.WriteLine("The first 10 natural numbers are:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(i + " ");
+                sum += i;
+            }
+            Console.WriteLine("\nThe Sum is: " + sum);
+        }
+    }
+}
